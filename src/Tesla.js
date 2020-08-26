05a4ae2a7e1fea8346fa46bba37ded6770f09128
Alice Aekyung Huh <aklee213@gmail.com>
2020-08-26 13:07:15 -0400

    [Tesla] Add Tesla component and image

---
+++ src/Tesla.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import modelY from './model_y.jpeg';
+
+class Tesla extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Tesla</h1>
+                <img src={} alt="Model Y photo" />
+                <p>
+                    Model:Model Y
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Tesla
